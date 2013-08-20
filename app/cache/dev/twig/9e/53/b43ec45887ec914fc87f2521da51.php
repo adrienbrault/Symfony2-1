23022b3371b94c21b97bfd79f1bc1dacdab1949c
Guillaume Occuly <occuly.guillaume@gmail.com>
2013-08-20 03:16:39 +0200

    First commit

---
+++ app/cache/dev/twig/9e/53/b43ec45887ec914fc87f2521da51.php
@@ -0,0 +1,75 @@
+<?php
+
+/* AcmeSecurityBundle:Security:login.html.twig */
+class __TwigTemplate_9e53b43ec45887ec914fc87f2521da51 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 2
+        echo "
+
+<!doctype html>
+<html lang=\"en-US\">
+<head>
+    <meta charset=\"UTF-8\">
+    <title></title>
+</head>
+<body>
+";
+        // line 11
+        if ((isset($context["error"]) ? $context["error"] : $this->getContext($context, "error"))) {
+            // line 12
+            echo "    <div>";
+            echo twig_escape_filter($this->env, $this->getAttribute((isset($context["error"]) ? $context["error"] : $this->getContext($context, "error")), "message"), "html", null, true);
+            echo "</div>
+";
+        }
+        // line 14
+        echo "
+<form action=\"";
+        // line 15
+        echo $this->env->getExtension('routing')->getPath("login_check");
+        echo "\" method=\"post\">
+    <label for=\"username\">Login :</label>
+    <input type=\"text\" id=\"username\" name=\"_username\" value=\"";
+        // line 17
+        echo twig_escape_filter($this->env, (isset($context["last_username"]) ? $context["last_username"] : $this->getContext($context, "last_username")), "html", null, true);
+        echo "\" />
+
+    <label for=\"password\">Mot de passe :</label>
+    <input type=\"password\" id=\"password\" name=\"_password\" />
+
+    ";
+        // line 27
+        echo "
+    <button type=\"submit\">login</button>
+</form>
+</body>
+</html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "AcmeSecurityBundle:Security:login.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  54 => 27,  46 => 17,  41 => 15,  38 => 14,  32 => 12,  30 => 11,  19 => 2,);
+    }
+}
